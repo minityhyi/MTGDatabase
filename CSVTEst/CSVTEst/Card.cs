2552d8765910a27e2f18826ad63bdff77659a8d0
minityhyi <147266406+minityhyi@users.noreply.github.com>
2024-04-21 14:55:19 +0200

    Revert "hej"
    This reverts commit e78fd32a61fe401af5f486ea46a552100bce6bbe.

---
+++ CSVTEst/CSVTEst/Card.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper.Configuration.Attributes;
+namespace CSVTEst;
+
+public class Card
+{
+    [Name("name")]
+    public string? Name { get; set; }
+
+    [Name("manaValue")]
+    public string? CMC { get; set; }
+
+    [Name("color")]
+    public string? Color { get; set; }
+
+    [Name("type")]
+    public string? Type { get; set; }
+
+    [Name("subType")]
+    public string? Subtype { get; set; }
+
+    [Name("power")]
+    public string? Power { get; set; }
+
+    [Name("toughness")]
+    public string? Toughness { get; set; }
+
+    [Name("loyalty")]
+    public string? Loyalty { get; set; }
+}
+
---
+++ CSVTEst/CSVTEst/Program.cs
@@ -0,0 +1,29 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using System.Globalization;
+using CSVTEst;
+
+string inputFile = @"C:\Users\andreas pc\Documents\GitHub\MTGDatabase\AllPrintingsSCV\cards.csv";
+string outputFile = @"C:\Users\andreas pc\Documents\GitHub\MTGDatabase\AllPrintingsSCV\filtered-cards.csv";
+List<Card> outputRecords = new();
+
+using var reader = new StreamReader(inputFile);
+using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+    HeaderValidated = null,
+    MissingFieldFound = null
+
+});
+
+var records = csv.GetRecords<Card>();
+
+
+int x = 0;
+foreach (var record in records)
+{
+    if(record.Name.StartsWith("S"))
+    {
+        Console.WriteLine(record.Name);
+    }
+}
